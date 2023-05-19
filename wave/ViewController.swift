8bbcceeaa458be62d074480c83667633425fd27a
athenian <bivboh@gmail.com>
2023-05-19 14:09:57 -0700

    Initial Commit

---
+++ wave/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wave
+//
+//  Created by athenian on 5/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
